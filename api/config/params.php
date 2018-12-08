50e156d0d1941f7b1d157af7e4d076e35e8a4285
desaikalpesh34 <desaikalpesh34@gmail.com>
2018-12-08 16:45:46 +0530

    remove extra plugin

--- api/config/params.php
+++ api/config/params.php
@@ -1,7 +1,4 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'baseurl'=>'http://votingapp.dev/',
-    'categoryurl'=>'http://votingapp.dev/storage/category/',
-    'storageurl'=>'http://votingapp.dev/storage/',
 ];
